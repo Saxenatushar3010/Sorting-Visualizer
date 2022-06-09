6e84a11996646ea0f193448e4af0bdae812aace4
Tushar Saxena <Saxenatushar3010@gmail.com>
2022-06-09 23:07:57 +0530

    first commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Footer Component
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                Created with <i className="fa fa-heart" style={{color: "#FF5252"}}></i> by
+                <a href="https://www.linkedin.com/in/tushar-saxena-3348881a3/" target="_blank" rel="noopener noreferrer" className="link">
+                    <span> </span>
+                    Tushar Saxena
+                </a>
+            </p>
+        </footer>
+    );
+}
+ 
+export default Footer;
